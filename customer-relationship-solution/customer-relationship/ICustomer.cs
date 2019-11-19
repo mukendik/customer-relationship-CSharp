d4f10ef1b88c7be60d9f1363e945c96b612af790
MUKENDI KALONJI GHISLAIN <mukendik@gmail.com>
2019-11-19 15:48:36 +0100

    add interfaces files

---
+++ customer-relationship-solution/customer-relationship/ICustomer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace customer_relationship
+{
+    interface ICustomer
+    {
+        IEnumerable<IOrder> PreviousOrders { get; }
+        DateTime DateJoined { get; }
+        DateTime? LastOrder { get; }
+        string Name { get; }
+        IDictionary<DateTime, string> Reminders { get; }
+    }
+}

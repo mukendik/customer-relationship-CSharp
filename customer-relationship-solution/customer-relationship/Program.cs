6fdc383a178c49a9eae444cb10007fd17c5f6422
MUKENDI KALONJI GHISLAIN <mukendik@gmail.com>
2019-11-19 15:36:46 +0100

    add gitignore file

---
+++ customer-relationship-solution/customer-relationship/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace customer_relationship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

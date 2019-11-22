189d2e8b89db3ae9bd936a4ff235865336541c4c
MUKENDI KALONJI GHISLAIN <mukendik@gmail.com>
2019-11-22 16:55:30 +0100

    survey

---
+++ customer-relationship-solution/customer-relationship/NullableIntroduction/survey/SurveyResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace customer_relationship.NullableIntroduction.survey
+{
+    class SurveyResponse
+    {
+        public int Id { get; }
+        public SurveyResponse(int id) => Id = id;
+    }
+}

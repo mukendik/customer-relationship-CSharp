0a23b1a820816d941b3fd300aabd2334ca15a00c
MUKENDI KALONJI GHISLAIN <mukendik@gmail.com>
2019-11-22 15:08:45 +0100

    add survey run class

---
+++ customer-relationship-solution/customer-relationship/NullableIntroduction/survey/SurveyRun.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace customer_relationship.NullableIntroduction.survey
+{
+    class SurveyRun
+    {
+        private List<SurveyQuestion> surveyQuestions = new List<SurveyQuestion>();
+        public void AddQuestion(QuestionType type, string question) =>
+        AddQuestion(new SurveyQuestion(type, question));
+        public void AddQuestion(SurveyQuestion surveyQuestion) => surveyQuestions.Add(surveyQuestion);
+    }
+}

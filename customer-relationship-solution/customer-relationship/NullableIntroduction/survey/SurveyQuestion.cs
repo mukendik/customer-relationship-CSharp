c13754cb18ae934ac3899bcfc57b7b182cc58733
MUKENDI KALONJI GHISLAIN <mukendik@gmail.com>
2019-11-22 14:49:16 +0100

    test

---
+++ customer-relationship-solution/customer-relationship/NullableIntroduction/survey/SurveyQuestion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace customer_relationship.NullableIntroduction.survey
+{
+    public enum QuestionType
+    {
+        YesNo,
+        Number,
+        Text
+    }
+
+    public class SurveyQuestion
+    {
+
+    }
+}
